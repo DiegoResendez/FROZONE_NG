9e9b5971666739bafe6c0827531ceeff06fc2050
unknown <diego.resendez@yahoo.com>
2020-10-30 17:16:40 -0700

    Buttons and Hide Elements

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // Get elementByID for the buttons and the other shit.
+const startButton = document.getElementById("startButton")
+const giftButton = document.getElementById("giftButton")
+const giftNameButton = document.getElementById("giftNameButton")
+const getPlayaNameButton = document.getElementById("getPlayaName")
+const newUserInformationElement = document.getElementById("newUserInformation");
+const nameDonationElement = document.getElementById("nameDonation");
+
+
+giftButton.addEventListener("click", giftMyName);
+startButton.addEventListener("click", getUserInfo);
+giftNameButton.addEventListener("click", addPlayaNameToDB);
+getPlayaName.addEventListener("click", startNameGenerator);
+
+
+function getUserInfo() 
+{
+    // startTimer();
+    newUserInformationElement.classList.remove("hide");
+    // questionContainerElement.classList.remove("hide");
+    // setNextQuestion();
+    console.log("Go to Start screen")
+}
+
+function giftMyName()
+{
+    nameDonationElement.classList.remove("hide");
+    // questionContainerElement.classList.remove("hide");
+
+    console.log("Go to gift screen")
+}
+
+function addPlayaNameToDB() 
+{
+    console.log("User gifts Playa name to others")
+}
+
+function startNameGenerator()
+{
+    console.log("Go to Questions Bank")
+}
